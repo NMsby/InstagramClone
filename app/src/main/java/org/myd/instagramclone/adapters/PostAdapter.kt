31f683646b4db010cae554b6bc908c0c4e34072c
Nelson Masbayi <nelsonmasbayi@gmail.com>
2024-11-08 11:28:40 +0300

    Update to HomePage Activities, Adapters, RecycleView

--- app/src/main/java/org/myd/instagramclone/adapters/PostAdapter.kt
+++ app/src/main/java/org/myd/instagramclone/adapters/PostAdapter.kt
@@ -1,6 +1,7 @@
 package org.myd.instagramclone.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -29,20 +30,37 @@ class PostAdapter(var context : Context, private var postList : ArrayList<Post>)
     }
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
-        Firebase.firestore.collection(USER_NODE).document(postList[position].uid).get().addOnSuccessListener {
-            val user = it.toObject<User>()
-            Glide.with(context).load(user!!.image).placeholder(R.drawable.user).into(holder.binding.profileImage)
-            holder.binding.name.text = user.name
+        try {
+            Firebase.firestore.collection(USER_NODE).document(postList[position].uid).get()
+                .addOnSuccessListener {
+                    val user = it.toObject<User>()
+                    Glide.with(context).load(user!!.image).placeholder(R.drawable.user)
+                        .into(holder.binding.profileImage)
+                    holder.binding.name.text = user.name
+                }
+        } catch (_:Exception) {
+
         }
         Glide.with(context).load(postList[position].postUrl).placeholder(R.drawable.loading).into(holder.binding.postImage)
+        try {
+            val text = TimeAgo.using(postList[position].time.toLong())
 
-        val text = TimeAgo.using(postList.get(position).time.toLong())
+            holder.binding.time.text = text
 
-        holder.binding.time.text = text
+        } catch (e:Exception) {
+            holder.binding.time.text=""
+        }
+
+        holder.binding.share.setOnClickListener {
+            var i = Intent(Intent.ACTION_SEND)
+            i.type = "text/plain"
+            i.putExtra(Intent.EXTRA_TEXT, postList[position].postUrl)
+            context.startActivity(i)
+        }
+
         holder.binding.caption.text = postList[position].caption
         holder.binding.like.setOnClickListener {
             holder.binding.like.setImageResource(R.drawable.red_like)
         }
-
     }
 }
